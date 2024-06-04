9ee758f3c09956bda2e91312b16a91ce4127c3cb
Alex <45000671+Alexti2d@users.noreply.github.com>
2024-06-04 11:08:00 +0200

    ajout de la librairie midi

---
+++ README.md
@@ -0,0 +1,12 @@
+# Installation
+
+https://chocolatey.org/install
+
+```
+choco install -y visualstudio2019buildtools visualstudio2019-workload-vctools
+npm config set msvs_version 2019
+```
+
+```
+npm install
+```
--- index.js
+++ index.js
@@ -2,26 +2,37 @@ const j = require("@gnaudio/jabra-node-sdk");
 j.createJabraApplication('JabraMidiController').then((jabra) => { // JabraMidiController is appID here
     jabra.on('attach', (device) => {
         console.log('Press any key on Jabra device ' + device.deviceName);
-        // device.getSupportedButtonEventsAsync().then((data) => {console.log('test', data)})
-        // device.setRemoteMmiActionAsync(j.enumRemoteMmiType.MMI_TYPE_LED_BUSYLIGHT, j.enumRemoteMmiSequence.MMI_LED_SEQUENCE_ON)
 
-        // If you are creating a softphone, consider using GN protocol when device supports it
-        // in order to receive all events. E.g. device.setHidWorkingStateAsync(j.enumHidState.GN_HID);
-        device.
-        // device.on('btnPress', (btnType, btnValue) => {
-        //   // console.log(j.enumDeviceBtnType)
-        //   // console.log('New input from device is received: ', device.setHidWorkingStateAsync(j.enumHidState.GN_HID));
-        //   console.log('New input from device is received: ', j.enumDeviceBtnType[btnType], btnValue);
-        //   if (j.enumDeviceBtnType[btnType] == "OffHook" && btnValue == true) {
-        //     console.log("PC || Start/Stop")
-        //   }
-        //   else if (j.enumDeviceBtnType[btnType] == "LineBusy" && btnValue == true) {
-        //     console.log("Tel Banc || Start Rec")
-        //   }
-        //   else if (j.enumDeviceBtnType[btnType] == "LineBusy" && btnValue == false) {
-        //     console.log("Tel Vert || Stop Rec")
-        //   }
-        // });
+        // device.offhookAsync(true)
+
+
+        device.on('btnPress', (btnType, btnValue) => {
+          // console.log(j.enumDeviceBtnType)
+          // console.log(j.DeviceEventsList)
+          console.log("\n")
+          // console.log('New input from device is received: ', j.enumDeviceBtnType[btnType], btnValue);
+          if (j.enumDeviceBtnType[btnType] == "OffHook" && btnValue == true) {
+            console.log("PC || Start")
+            device.offhookAsync(true)
+            
+          }
+          else if (j.enumDeviceBtnType[btnType] == "OffHook" && btnValue == false) {
+            console.log("PC || Stop")
+            device.onhookAsync(false)
+          }
+          else if (j.enumDeviceBtnType[btnType] == "LineBusy" && btnValue == true) {
+            console.log("Tel || Start Solo")
+          }
+          else if (j.enumDeviceBtnType[btnType] == "LineBusy" && btnValue == false) {
+            console.log("Tel || Stop Solo")
+          }
+          else if (j.enumDeviceBtnType[btnType] == "Mute" && btnValue == true) {
+            console.log("Mute || Start Rec")
+          }
+          else if (j.enumDeviceBtnType[btnType] == "Unmute" && btnValue == true) {
+            console.log("Mute || Stop Rec")
+          }
+        });
     });   
     jabra.on('detach', (device) => {
       console.log('deconnecte')
